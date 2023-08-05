5eeb80908423f693eba76904b8b614dc945ef0a3
Aydar <bilyalov3003@gmail.com>
2023-08-05 20:00:50 +0600

    create app component

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class AppHeader extends React.Component {
+    render() {
+        return(
+            <div className="app-header d-flex">
+                <h1>Aidar B</h1>
+                <h2>5 posts, 2 liked</h2>
+            </div>
+        )
+    }
+}
