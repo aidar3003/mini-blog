ee1badc7b8bf58c0fdc1eed254c9192cd4631004
Aydar <bilyalov3003@gmail.com>
2023-08-09 04:40:22 +0600

    added onLike function

--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -6,9 +6,11 @@ export default class PostListItem extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            important: false
+            important: false,
+            like: false
         }
         this.onImportant = this.onImportant.bind(this)
+        this.onLike = this.onLike.bind(this);
     }
 
     onImportant() {
@@ -16,10 +18,15 @@ export default class PostListItem extends Component {
             important: !prevState.important
         }))
     }
+    onLike() {
+        this.setState(prevState => ({
+            like: !prevState.like
+        }))
+    }
 
     render() {
-        const {label, like} = this.props;
-        const {important} = this.state;
+        const {label} = this.props;
+        const {important, like} = this.state;
 
         let classNames = "app-list-item d-flex justify-content-between";
             if(important) {
@@ -31,7 +38,7 @@ export default class PostListItem extends Component {
 
         return(
             <li className={classNames}>
-            <span className="app-list-item-label">{label}</span>
+            <span className="app-list-item-label" onClick={this.onLike}>{label}</span>
             <div className="d-flex justify-content-center align-items-center">
                <button type="button" className="btn-star btn-sm" onClick={this.onImportant}>
                 <i className="fa fa-star"></i>
